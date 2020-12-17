f54bcc535646ca000100aa7d110addb2f0ac3f5c
Huong Dang <miley.dawn@codeimmersives.com>
2020-12-17 15:38:54 -0500

    create all functional elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react'
+import { uuid } from 'uuidv4';
 
 function App() {
+
+let initialArray = window.localStorage.getItem("todos")
+? window.localStorage.getItem("todos")
+: []
+  
+  const [todoList, setTodoList] = useState(initialArray)
+  const [inputValue, setInputValue] = useState("")
+
+  function addTodo(e) {
+    e.preventDefault()
+    if (inputValue.length !== 0) {
+      let newTodoList = [
+        ...todoList,
+        {
+          id: uuid(),
+          name: inputValue,
+          isComplete: false
+        }
+      ]
+      setTodoList(newTodoList)
+    }
+    setInputValue("")
+  }
+
+  function handleDone(item) {
+    if (item.isComplete === true) {
+      item.isComplete = false
+    }
+    else if (item.isComplete === false) {
+      item.isComplete = true
+    }
+    let newTodoList = [...todoList]
+    setTodoList(newTodoList)
+  }
+
+  function handleDelete(deleteItem){
+    let newTodoList = [...todoList]
+    const afterDeleteTodo = newTodoList.filter(item => item!==deleteItem)
+    setTodoList(afterDeleteTodo)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      this is the todo app
+      <form onSubmit={addTodo}>
+        <input
+          onChange={(e) => setInputValue(e.target.value)}
+        ></input>
+        <button>Add</button>
+
+        {todoList.map(item => {
+          return (
+            <>
+              <li
+                className={item.isComplete ? 'completed-task' : ''}
+                key={item.id}>{item.name}</li>
+              <button
+                onClick={() => handleDone(item)}
+              >done</button>
+              <button
+                onClick={() => handleDelete(item)}
+              >delete</button>
+              <hr />
+            </>
+          )
+        })}
+      </form>
     </div>
   );
 }
